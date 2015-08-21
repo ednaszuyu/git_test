ac674056bc1abf8fb7f12f7d9421fb82c69fd72a
fg <fg@fgtekiMBP-2.lan>
2015-08-21 16:10:31 +0800

    abbey

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+	$a = "tom";
+$b = "lalalala";
+$c = "i do nothing for u....";
+
+git initial
+
+git status//看狀態
+
+git add.
+
+git comit
+
+git pull
+
+git push
+
+git reset head^
+
+git clone網址 //載別人的
+
+----------------------------------------------------
+
+cat
+
+vi編輯器
+
+詳細筆記請洽eva or abbey
+edna感覺來到外太空聽了外星語@）％＄@3*$+_!(j9(u8@&!
+wendy fighting!!!!
+
+
+?>
